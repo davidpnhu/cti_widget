27e174a21740161c385d6d8de7b59fc36df6e859
davidpnhu <david.nhu@blood.ca>
2026-05-27 14:15:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function handleAccept() {
   debugger;
   var auth = "Basic " + $('#auth').val();
+  //"https://canbs-ccx-pub.internal.bloodservices.ca:8445/finesse/api/User/1002005/Dialogs"
+  var url = "https://my1002373.us1.test.crm.cloud.sap/sap/c4c/api/v1/account-service/accounts?$filter=defaultCommunication/phoneNormalisedNumber%20eq%20%27%2B16131112222%27&$select=displayId,isNaturalPerson";
   $.ajax({
-    url: "https://canbs-ccx-pub.internal.bloodservices.ca:8445/finesse/api/User/1002005/Dialogs",
+    url: url,
     method: "GET",
     headers: {
       "Authorization": auth
